25ff2a2dfb228e1e57e50cebff10fcf37e07fcd3
luodan01 <69646558+luodan01@users.noreply.github.com>
2021-06-28 14:54:53 +0800

    repositioned heading

--- website/src/firebaseConfig.ts
+++ website/src/firebaseConfig.ts
@@ -1,14 +1,5 @@
 import firebase from "firebase";
 
-/*const firebaseConfig = {
-  apiKey: "AIzaSyBc0r99P9Mdeme1JlfotgpnuiVcOxghfmE",
-  authDomain: "htwoo-module-planner.firebaseapp.com",
-  projectId: "htwoo-module-planner",
-  storageBucket: "htwoo-module-planner.appspot.com",
-  messagingSenderId: "1097132220327",
-  appId: "1:1097132220327:web:ad0ab52f792cc9a90fa082",
-};
-*/
 
 const firebaseConfig = {
   apiKey: "AIzaSyCg3YGBYhcmAB5tkgaC6V3wQf-b8uy8bVc",
@@ -18,6 +9,7 @@ const firebaseConfig = {
   messagingSenderId: "463105634154",
   appId: "1:463105634154:web:7697aee6e59bf1f8537f13"
   };
+  
 firebase.initializeApp(firebaseConfig);
 
 export default firebase;
